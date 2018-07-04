1d6b368ddea5e3f70b3e1f76208c02a6a30cb804
David Ham <jianguang.ran@gmail.com>
2018-07-03 18:02:20 -0600

    established working channel_layer backed by redis

---
+++ static/scripts/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import DataComponent from './app'
+
+const rootContainer = document.getElementById('reactContainer')
+
+ReactDOM.render(
+    <dataComponent/>,
+    rootContainer
+)
